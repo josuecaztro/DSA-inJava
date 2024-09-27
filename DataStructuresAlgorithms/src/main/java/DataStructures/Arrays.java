b3501471bc537a3987cc661a082c438c878f2c3a
Josue Castro <josuecaztro@gmail.com>
2024-09-27 17:59:37 -0400

    dsa for arrays created

--- DataStructuresAlgorithms/src/main/java/DataStructures/Arrays.java
+++ DataStructuresAlgorithms/src/main/java/DataStructures/Arrays.java
@@ -2,6 +2,21 @@ package DataStructures;
 
 public class Arrays {
 
-    //testing 123 123
+    public static int[] exampleArray = {5, 90, 23, 4, 17, 834, 38, 2};
+
+    public static int findLowestValue(int[] arr){
+        int solution = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i] < solution){
+                solution = arr[i];
+            }
+        }
+        System.out.println("The lowest value is: " + solution);
+        return solution;
+    }
+
+    public static void main(String[] args) {
+        findLowestValue(exampleArray);
+    }
 
 }
