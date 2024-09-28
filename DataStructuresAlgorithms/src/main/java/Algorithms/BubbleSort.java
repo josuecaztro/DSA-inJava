d80fa33a2d035964d341e05923912f08707dabc5
Josue Castro <josuecaztro@gmail.com>
2024-09-28 16:27:45 -0400

    bubble sort off memory

---
+++ DataStructuresAlgorithms/src/main/java/Algorithms/BubbleSort.java
@@ -0,0 +1,23 @@
+package Algorithms;
+
+public class BubbleSort {
+
+    public static int[] exampleArr = {6,90,2,4,18,753,31,7};
+
+    public static int[] bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr.length; j++){
+                if (arr[j] > arr[i]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        bubbleSort(exampleArr);
+    }
+}
