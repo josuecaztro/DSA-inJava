591025ba2f5605c4e7afd81dac2ced191ea039ea
Josue Castro <josuecaztro@gmail.com>
2024-10-01 17:17:56 -0400

    minor sort fixes

--- DataStructuresAlgorithms/src/main/java/Algorithms/SelectionSort.java
+++ DataStructuresAlgorithms/src/main/java/Algorithms/SelectionSort.java
@@ -2,21 +2,23 @@ package Algorithms;
 
 public class SelectionSort {
 
-    public static int[] exampleArr = {6,90,2,4,18,753,31,7};
+    public static int[] exampleArr = {6,90,2,4,18,1,753,31,7};
 
     public static int[] selectionSort(int[] arr){
-      int min;
-      for (int i = 0; i < arr.length; i++){
-          min = i;
-          for (int j = i + 1; j < arr.length; j++){
-              if (arr[j] < arr[min]){
-                  min = j;
-              }
-          }
-          int temp = arr[i];
-          arr[i] = arr[min];
-          arr[min] = temp;
-      }
+
+        int lowest;
+
+        for (int i = 0; i < arr.length; i++){
+            lowest = i; //remember its i not arr[i]
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] < arr[lowest]){
+                    lowest = j; //remember its j not arr[j]
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
 
       for (int x : arr){
           System.out.print(x + ", ");
