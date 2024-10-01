ad4cbc9cb8887c22d75fb00d3f51152e887a8a35
Josue Castro <josuecaztro@gmail.com>
2024-10-01 17:10:07 -0400

    revised correct conventional selection sort

--- DataStructuresAlgorithms/src/main/java/Algorithms/SelectionSort.java
+++ DataStructuresAlgorithms/src/main/java/Algorithms/SelectionSort.java
@@ -2,25 +2,26 @@ package Algorithms;
 
 public class SelectionSort {
 
-    public static int[] exampleArr = {6,90,2,40,18,753,31,7};
+    public static int[] exampleArr = {6,90,2,4,18,753,31,7};
 
     public static int[] selectionSort(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++){
-            int lowest = arr[i];
-            for (int j = i; j < arr.length - 1; j++){
-                if (arr[j + 1] < lowest){
-                    int temp = lowest;
-                    lowest = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-            arr[i] = lowest;
-        }
+      int min;
+      for (int i = 0; i < arr.length; i++){
+          min = i;
+          for (int j = i + 1; j < arr.length; j++){
+              if (arr[j] < arr[min]){
+                  min = j;
+              }
+          }
+          int temp = arr[i];
+          arr[i] = arr[min];
+          arr[min] = temp;
+      }
 
-        for (int x : arr){
-            System.out.print(x + ", ");
-        }
-        return arr;
+      for (int x : arr){
+          System.out.print(x + ", ");
+      }
+      return arr;
     }
 
     public static void main(String[] args) {
